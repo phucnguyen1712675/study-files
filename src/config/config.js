bfd27bcca8344af276a76074cf060c21d86d55ac
Sammatha99 <ngonhatrang99@gmail.com>
2021-07-19 17:03:42 +0700

    heroku

--- src/config/config.js
+++ src/config/config.js
@@ -26,11 +26,13 @@ const envVarsSchema = Joi.object()
   })
   .unknown();
 
-const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+// const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
 
-if (error) {
-  throw new Error(`Config validation error: ${error.message}`);
-}
+// if (error) {
+//   throw new Error(`Config validation error: ${error.message}`);
+// }
+
+const { value: envVars } = envVarsSchema.prefs({ errors: { label: 'key' } }); // .validate(process.env);
 
 module.exports = {
   env: envVars.NODE_ENV,
