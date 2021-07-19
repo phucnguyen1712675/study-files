ea3acd481e07e2a996b6f2d89829e9f007fa7d8c
Sammatha99 <ngonhatrang99@gmail.com>
2021-07-19 17:23:47 +0700

    fix monogo url

--- src/config/config.js
+++ src/config/config.js
@@ -8,7 +8,8 @@ const envVarsSchema = Joi.object()
   .keys({
     NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
     PORT: Joi.number().default(3000),
-    MONGODB_URL: Joi.string().required().description('Mongo DB url'),
+    // MONGODB_URL: Joi.string().required().description('Mongo DB url'),
+    MONGODB_URL: Joi.string().description('Mongo DB url'),
     JWT_SECRET: Joi.string().required().description('JWT secret key'),
     JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
     JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
@@ -36,10 +37,10 @@ module.exports = {
   env: envVars.NODE_ENV,
   port: envVars.PORT,
   mongoose: {
-    url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
-    // url: `mongodb+srv://phucadmin:mimi1234@study-files-cluster.eu58v.mongodb.net/study-files?retryWrites=true&w=majority${
-    //   envVars.NODE_ENV === 'test' ? '-test' : ''
-    // }`,
+    // url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
+    url: `mongodb+srv://phucadmin:mimi1234@study-files-cluster.eu58v.mongodb.net/study-files?retryWrites=true&w=majority${
+      envVars.NODE_ENV === 'test' ? '-test' : ''
+    }`,
     options: {
       useCreateIndex: true,
       useNewUrlParser: true,
