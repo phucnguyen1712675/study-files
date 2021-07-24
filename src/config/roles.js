14392416a6ac9e9de1fac62ca3615c239e06b66c
phucnguyen1712675 <pbadboyv@gmail.com>
2021-07-24 12:14:46 +0700

    Fix teacher roles

--- src/config/roles.js
+++ src/config/roles.js
@@ -2,7 +2,7 @@ const roles = ['student', 'teacher', 'admin'];
 
 const roleRights = new Map();
 roleRights.set(roles[0], ['updateUser', 'getUser', 'manageMyCourses', 'manageWatchList']);
-roleRights.set(roles[1], ['updateCourse', 'addCourse', 'updateTeacher', 'getTeacher']);
+roleRights.set(roles[1], ['manageCourses', 'updateTeacher', 'getTeacher']);
 roleRights.set(roles[2], ['manageUsers', 'manageTeachers', 'manageCategories', 'getUser', 'getTeacher', 'deleteCourse']);
 
 module.exports = {
